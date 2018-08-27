ff5cf3f9a5bf3b3cc187e82a7601c02d5bf6990e
hjc <hjc@163.com>
2018-08-27 17:58:12 +0800

    百度语音接入

--- app/src/main/java/com/example/administrator/myapplication/NoDisplayActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/NoDisplayActivity.java
@@ -1,5 +1,6 @@
 package com.example.administrator.myapplication;
 
+import android.app.Activity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
@@ -7,6 +8,7 @@ import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
 import com.baidu.speech.EventListener;
 import com.baidu.speech.EventManager;
@@ -16,10 +18,11 @@ import com.example.administrator.util.AutoCheck;
 
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class NoDisplayActivity extends AppCompatActivity implements EventListener{
+public class NoDisplayActivity extends Activity implements EventListener{
 
     private EventManager asr;
 
@@ -35,24 +38,33 @@ public class NoDisplayActivity extends AppCompatActivity implements EventListene
         //时间一到跳转Activity,在这个Activity中播放音乐
         mediaPlayer = MediaPlayer.create(this, R.raw.one);
         mediaPlayer.start();
-        start();
+        try {
+            start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void start() {
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.finish();
+    }
+
+    public void start() throws IOException {
         Map<String, Object> params = new LinkedHashMap<String, Object>();
-        String event = null;
-        event = SpeechConstant.ASR_START; // 替换成测试的event
+        String event = SpeechConstant.ASR_START; // 替换成测试的event
 
         if (enableOffline) {
             params.put(SpeechConstant.DECODER, 2);
         }
         params.put(SpeechConstant.ACCEPT_AUDIO_VOLUME, false);
         // params.put(SpeechConstant.NLU, "enable");
-        // params.put(SpeechConstant.VAD_ENDPOINT_TIMEOUT, 0); // 长语音
-        // params.put(SpeechConstant.IN_FILE, "res:///com/baidu/android/voicedemo/16k_test.pcm");
-        // params.put(SpeechConstant.VAD, SpeechConstant.VAD_DNN);
+         params.put(SpeechConstant.VAD_ENDPOINT_TIMEOUT, 0); // 长语音
+//         params.put(SpeechConstant.IN_FILE, "res:///com/baidu/android/voicedemo/16k_test.pcm");
+         params.put(SpeechConstant.VAD, SpeechConstant.VAD_DNN);
         // params.put(SpeechConstant.PROP ,20000);
-        // params.put(SpeechConstant.PID, 1537); // 中文输入法模型，有逗号
+         params.put(SpeechConstant.PID, 1537); // 中文输入法模型，有逗号
         // 请先使用如‘在线识别’界面测试和生成识别参数。 params同ActivityRecog类中myRecognizer.start(params);
         // 复制此段可以自动检测错误
         (new AutoCheck(getApplicationContext(), new Handler() {
@@ -67,26 +79,44 @@ public class NoDisplayActivity extends AppCompatActivity implements EventListene
                 }
             }
         },enableOffline)).checkAsr(params);
-        String json = null; // 可以替换成自己的json
-        json = new JSONObject(params).toString(); // 这里可以替换成你需要测试的json
+        String json = new JSONObject(params).toString(); // 这里可以替换成你需要测试的json
         asr.send(event, json, null, 0, 0);
         Log.i("NoDisplayActivity","输入参数：" + json);
     }
 
     @Override
     public void onEvent(String name, String params, byte[] data, int offset, int length) {
-
+        boolean stopFlag = false;
         if (params != null && !params.isEmpty()) {
             Log.i(" ;params :" , params);
         }
         if (name.equals(SpeechConstant.CALLBACK_EVENT_ASR_PARTIAL)) {
+            Log.i("结果", params);
             if (params.contains("\"nlu_result\"")) {
                 if (length > 0 && data.length > 0) {
                     Log.i( ", 语义解析结果：" ,new String(data, offset, length));
                 }
             }
+            if (params.contains("\"final_result\"")) {
+                if (length > 0 && data.length > 0) {
+                    Log.i(",最终结果", new String(data, offset, length));
+                }
+            }
+            if(params.contains("\"results_recognition\"")){
+                if (length > 0 && data.length > 0) {
+                    Log.i(",第一个结果", new String(data, offset, length));
+                }
+            }
         } else if (data != null) {
             Log.i("NoDis"," ;data length=" + data.length);
         }
+        if (stopFlag) {
+            stop();
+        }
+    }
+
+    public void stop() {
+        mediaPlayer.stop();
+        finish();
     }
 }
